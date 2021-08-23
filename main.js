c5365e1c13b036a9d200199ff10301285ee9cfb3
avanthikanair9 <75365203+avanthikanair9@users.noreply.github.com>
2021-08-23 17:32:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function setup(){
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+}
+
+function modelLoaded()
+{
+ console.log('Model Loaded!');
+ }
+
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocssd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
